b1ca4a406ee5675d26d59dc75e20a205f27c4082
Swassy <swassyman2005@gmail.com>
2025-02-15 10:44:58 +0530

    fetch and add tourns

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,35 +11,99 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> tournamentName = [];
+  List<Tournament> tournamentList = [];
 
-  void _addItem() {
-    setState(() {
-      tournamentName.add('Items ${tournamentName.length + 1}');
-    });
+  @override
+  void initState() {
+    super.initState();
+    _fetchTournamentList();
   }
 
-  Widget _bodyWidget() {
-    return Scaffold(
-      body: ListView.builder(
-          itemCount: tournamentName.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              //thing of the title
-              title: Text(tournamentName[index]),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addItem,
-        child: Icon(Icons.add),
-      ),
-    );
+  Future<void> _fetchTournamentList() async {
+    try {
+      final url = Uri.parse('http://127.0.0.1:5000/tournaments');
+      final response = await http.get(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        //successful fetching
+        final data = json.decode(response.body);
+        setState(() {
+          tournamentList = List<Tournament>.from(data['tournaments']
+              .map((tournamentData) => Tournament.fromJson(tournamentData)));
+        });
+      } else {
+        print("Failed to load tournament list");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _addTournament(String name) {
+    try {
+      final url = Uri.parse('http://127.0.0.1:5000/tournaments/add');
+      final response = await http.post(
+        url, 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: json.encode(
+          {
+            'name' : name,
+          }
+        ),
+      );
+
+      if(response.statusCode == 200) {
+        print("Tournament created successfully");
+      } else {
+        print("Failed to add tournament");
+      }
+    }
+    catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _bodyWidget(),
+    );
+  }
+
+  Widget _bodyWidget() {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: tournamentList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              //thing of the title
+              title: Text(tournamentList[index].),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addItem,
+        child: Icon(Icons.add),
+      ),
+      backgroundColor: Color(0xFF212121),
+    );
+  }
+}
+
+class Tournament {
+  final String name;
+
+  Tournament({required this.name);
+
+  factory Tournament.fromJson(Map<String, dynamic> json) {
+    return Tournament(
+      name: json['name'],
     );
   }
 }
