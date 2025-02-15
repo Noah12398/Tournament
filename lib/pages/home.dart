5522c32bee63d83267eb71ba7db2935b34c8b5cb
Swassy <swassyman2005@gmail.com>
2025-02-15 13:14:18 +0530

    minor fixes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,23 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tournament App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
